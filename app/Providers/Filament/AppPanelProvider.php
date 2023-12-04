7f8d0f23058c9a36a3f20a01b8ae9624fea144e0
Andy Newhouse <hi@andymnewhouse.me>
2023-12-04 10:55:39 -0600

    Add favicon

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -59,6 +59,15 @@ class AppPanelProvider extends PanelProvider
             ->sidebarCollapsibleOnDesktop(true)
             ->sidebarWidth('18em')
             ->profile()
-            ->plugin(SimpleLightBoxPlugin::make());
+            ->plugin(SimpleLightBoxPlugin::make())
+            ->renderHook(
+                'panels::head.start',
+                fn (): string => '<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+                    <link rel="manifest" href="/site.webmanifest">
+                    <meta name="msapplication-TileColor" content="#2b5797">
+                    <meta name="theme-color" content="#ffffff">',
+            );
     }
 }
