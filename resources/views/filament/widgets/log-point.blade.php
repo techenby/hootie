59a2e1f90bce3cbc0dbff53990ad5955f9188287
Andy Newhouse <hi@andymnewhouse.me>
2023-12-24 11:10:39 -0600

    Add log point widget

---
+++ resources/views/filament/widgets/log-point.blade.php
@@ -0,0 +1,17 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+        <div class="flex items-center gap-x-3">
+            <div class="flex-1">
+                <h2
+                    class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white"
+                >
+                    Log Delta Point
+                </h2>
+            </div>
+
+            {{ $this->createAction() }}
+
+            <x-filament-actions::modals />
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget>
