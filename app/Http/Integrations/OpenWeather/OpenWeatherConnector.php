f880d59f0d07b32cd23d85ca7d4b4de5ab67191c
Andy Newhouse <hi@andymnewhouse.me>
2024-02-14 11:14:23 -0600

    Add OpenWeather stuff

---
+++ app/Http/Integrations/OpenWeather/OpenWeatherConnector.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Integrations\OpenWeather;
+
+use Saloon\Http\Auth\QueryAuthenticator;
+use Saloon\Http\Connector;
+use Saloon\Traits\Plugins\AcceptsJson;
+
+class OpenWeatherConnector extends Connector
+{
+    use AcceptsJson;
+
+    public function __construct(public readonly string $token)
+    {
+    }
+
+    public function resolveBaseUrl(): string
+    {
+        return 'http://api.openweathermap.org';
+    }
+
+    protected function defaultAuth(): QueryAuthenticator
+    {
+        return new QueryAuthenticator('appid', $this->token);
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [];
+    }
+
+    protected function defaultConfig(): array
+    {
+        return [];
+    }
+}
