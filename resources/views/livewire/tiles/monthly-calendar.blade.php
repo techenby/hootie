cbfb934083aaef438732b5dfd9768c1bc45d11f7
Andy Newhouse <hi@andymnewhouse.me>
2024-02-16 10:55:04 -0600

    Style monthly-calendar

--- resources/views/livewire/tiles/monthly-calendar.blade.php
+++ resources/views/livewire/tiles/monthly-calendar.blade.php
@@ -32,24 +32,37 @@ new class extends Component {
     }
 }; ?>
 
-<x-tile class="grid grid-cols-7 gap-1" :width="$data['width']" :height="$data['height']">
-    <button wire:click="prevMonth" class="p-1 text-2xl text-center">&laquo;</button>
-    <div class="text-2xl p-1 col-span-5 text-center">{{ $month }}</div>
-    <button wire:click="nextMonth" class="p-1 text-2xl text-center">&raquo;</button>
+<x-tile class="text-center" :width="$data['width']" :height="$data['height']">
+    <div class="flex items-center text-gray-900">
+        <button wire:click="prevMonth" type="button" class="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500">
+            <span class="sr-only">Previous month</span>
+            <x-heroicon-o-chevron-left class="size-5" />
+        </button>
+        <div class="flex-auto text-sm font-semibold">{{ $month }}</div>
+        <button wire:click="nextMonth" type="button" class="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500">
+            <span class="sr-only">Next month</span>
+            <x-heroicon-o-chevron-right class="h-5 w-5" />
+        </button>
+    </div>
+    <div class="grid grid-cols-7 gap-1 mt-6 text-xs text-gray-500">
+        <div abbr="Sunday" scope="col" title="Sunday">S</div>
+        <div abbr="Monday" scope="col" title="Monday">M</div>
+        <div abbr="Tuesday" scope="col" title="Tuesday">T</div>
+        <div abbr="Wednesday" scope="col" title="Wednesday">W</div>
+        <div abbr="Thursday" scope="col" title="Thursday">T</div>
+        <div abbr="Friday" scope="col" title="Friday">F</div>
+        <div abbr="Saturday" scope="col" title="Saturday">S</div>
+    </div>
+    <div class="isolate mt-2 grid grid-cols-7 gap-px text-sm ">
+        @if ($dayOfWeek !== 0)
+        <!-- could be col-span-1 col-span-2 col-span-3 col-span-4 col-span-5 col-span-6 -->
+        <div class="col-span-{{ $dayOfWeek }}"></div>
+        @endif
 
-    <div abbr="Sunday" scope="col" title="Sunday" class="text-center text-gray-700">S</div>
-    <div abbr="Monday" scope="col" title="Monday" class="text-center text-gray-700">M</div>
-    <div abbr="Tuesday" scope="col" title="Tuesday" class="text-center text-gray-700">T</div>
-    <div abbr="Wednesday" scope="col" title="Wednesday" class="text-center text-gray-700">W</div>
-    <div abbr="Thursday" scope="col" title="Thursday" class="text-center text-gray-700">T</div>
-    <div abbr="Friday" scope="col" title="Friday" class="text-center text-gray-700">F</div>
-    <div abbr="Saturday" scope="col" title="Saturday" class="text-center text-gray-700">S</div>
-
-    @if ($dayOfWeek !== 0)
-    <div class="col-span-{{ $dayOfWeek }}"></div>
-    @endif
-
-    @foreach (range(1, $daysInMonth) as $date)
-    <div class="text-center {{ $date === $todaysDate ? 'rounded-md border border-gray-500' : '' }}">{{ $date }}</div>
-    @endforeach
+        @foreach (range(1, $daysInMonth) as $date)
+        <div class="{{ $date === $todaysDate ? 'rounded-md border border-gray-500' : '' }}">
+            <span class="mx-auto flex size-7 items-center justify-center">{{ $date }}</span>
+        </div>
+        @endforeach
+    </div>
 </x-tile>
