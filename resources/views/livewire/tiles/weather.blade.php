3787680bd12578eb11387bcf2a264b8b1ccfc095
Andy Newhouse <hi@andymnewhouse.me>
2024-03-04 21:01:48 -0600

    JKS

--- resources/views/livewire/tiles/weather.blade.php
+++ resources/views/livewire/tiles/weather.blade.php
@@ -13,38 +13,13 @@ new class extends Component {
     {
         return [
             'weather' => Tile::whereType('weather')->whereName($this->data['zip'])->first()->data,
+            'component' => match($this->data['width'] . 'x' . $this->data['height']) {
+                '2x1' => 'tiles.weather-2x1',
+                default => 'tiles.weather-1x1',
+            },
+            'name' => $this->data['name'],
         ];
     }
 }; ?>
 
-<x-tile :width="$data['width']" :height="$data['height']">
-    <div class="w-full flex items-start justify-between">
-        <p class="text-sm font-semibold">{{ $data['name'] }}</p>
-        <x-weather-icon class="size-6 dark:text-gray-200" :id="$weather['current']['weather'][0]['id']" />
-    </div>
-
-    <div class="w-full mt-2 flex items-end justify-between">
-        <div class="text-4xl dark:text-gray-200">{{ round($weather['current']['temp']) }}°</div>
-        <div class="flex">
-            <p class="text-gray-500 dark:text-gray-400">{{ round($weather['daily'][0]['temp']['min']) }}</p>
-            <p class="w-9 text-right dark:text-gray-200">{{ round($weather['daily'][0]['temp']['max']) }}°</p>
-        </div>
-    </div>
-
-    <div class="space-y-2 mt-2">
-        @foreach ($weather['daily'] as $index => $day)
-        @if ($index > 0 && $index < 5)
-        <div class="w-full flex justify-between text-sm">
-            <div class="flex">
-                <p class="w-12">{{ Carbon\Carbon::parse($day['dt'], $weather['timezone'])->format('D') }}</p>
-                <x-weather-icon class="size-5" :id="$day['weather'][0]['id']" />
-            </div>
-            <div class="flex">
-                <p class="text-gray-500 dark:text-gray-400">{{ round($day['temp']['min']) }}</p>
-                <p class="w-9 text-right dark:text-gray-200">{{ round($day['temp']['max']) }}°</p>
-            </div>
-        </div>
-        @endif
-        @endforeach
-    </div>
-</x-tile>
+<x-dynamic-component :component="$component" :weather="$weather" :name="$name" />
