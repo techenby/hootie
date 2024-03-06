ed95cfa23f4e91947e1f4c1224274a348f6849ba
Andy Newhouse <hi@andymnewhouse.me>
2024-03-05 20:59:55 -0600

    Add gap fix

--- resources/views/components/tile.blade.php
+++ resources/views/components/tile.blade.php
@@ -1,4 +1,4 @@
 @props(['width' => 1, 'height' => 1])
-<div {{ $attributes->merge(['class' => 'bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-400 p-2']) }} style="height: calc({{ $height }} * var(--bento-row-height))" data-bento="{{ $width }}x{{ $height }}">
+<div {{ $attributes->merge(['class' => 'bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-400 p-2']) }} style="height: calc({{ $height }} * var(--bento-row-height)) + {{ $height > 1 ? $height * 4 : 0 }}px" data-bento="{{ $width }}x{{ $height }}">
     {{ $slot }}
 </div>
