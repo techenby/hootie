c86a904b8ed69ee1b1c11257a85863dca820981d
Andy Newhouse <hi@andymnewhouse.me>
2024-03-08 12:10:33 -0600

    Add theme

--- resources/views/components/tiles/debug.blade.php
+++ resources/views/components/tiles/debug.blade.php
@@ -1,5 +1,5 @@
 <x-tile :width="$data['width']" :height="$data['height']">
-    <div x-data="{ innerWidth: window.innerWidth, innerHeight: window.innerHeight, outerWidth: window.outerWidth, outerHeight: window.outerHeight }">
+    <div x-data="{ innerWidth: window.innerWidth, innerHeight: window.innerHeight, outerWidth: window.outerWidth, outerHeight: window.outerHeight, theme: localStorage.theme }">
         <dl>
             <dt>Inner Width</dt>
             <dd x-text="innerWidth"></dd>
@@ -9,6 +9,8 @@
             <dd x-text="outerWidth"></dd>
             <dt>Outer Height</dt>
             <dd x-text="outerHeight"></dd>
+            <dt>Theme</dt>
+            <dd x-text="theme"></dd>
         </dl>
     </div>
 </x-tile>
