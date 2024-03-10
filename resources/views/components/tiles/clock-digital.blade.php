35522fc677ddd096fb2478c19cf4c40eaf377e03
Andy Newhouse <hi@andymnewhouse.me>
2024-03-10 13:01:14 -0500

    Fix digital clock

--- resources/views/components/tiles/clock-digital.blade.php
+++ resources/views/components/tiles/clock-digital.blade.php
@@ -1,7 +1,24 @@
 <x-tile class="flex items-center justify-center relative" :width="$data['width']" :height="$data['height']">
     <div class="flex-auto text-sm font-semibold absolute top-0 left-0 p-2">{{ now()->timezone($data['timezone'])->format('T') }}</div>
-    <div>
-        <p class="text-4xl font-bold mb-2 text-center dark:text-gray-200">{{ now()->timezone($data['timezone'])->format('g:i A') }}</p>
-        <p class="text-sm text-center">{{ now()->timezone($data['timezone'])->format('l F jS, Y') }}</p>
+    <div
+        x-data="{
+            timezone: @js($data['timezone']),
+            now: null,
+            time: null,
+            date: null,
+            init() {
+                let now = new Date()
+                this.time = now.toLocaleTimeString('en-US', { timeZone: this.timezone, timeStyle: 'short', })
+                this.date = now.toLocaleDateString('en-US', { timeZone: this.timezone, dateStyle: 'full', })
+                setInterval(() => {
+                    let now = new Date()
+                    this.time = now.toLocaleTimeString('en-US', { timeZone: this.timezone, timeStyle: 'short', })
+                    this.date = now.toLocaleDateString('en-US', { timeZone: this.timezone, dateStyle: 'full', })
+                }, 1000);
+            }
+        }"
+    >
+        <p x-text="time" class="text-4xl font-bold mb-2 text-center dark:text-gray-200"></p>
+        <p x-text="date" class="text-sm text-center"></p>
     </div>
 </x-tile>
