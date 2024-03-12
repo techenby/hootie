c8beecee222a4eabdd09e2b4862ffe3bc8c1b337
Shift <shift@laravelshift.com>
2024-03-12 14:39:27 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('board:fetch-calendar-events')->everyMinute();
+Schedule::command('board:fetch-weather')->everyFifteenMinutes();
+Schedule::command('board:fetch-pressure')->everyFifteenMinutes();
