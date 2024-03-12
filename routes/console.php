a235148dfdf3d237707b5a0f279e15a8d802ad15
Shift <shift@laravelshift.com>
2024-03-12 14:39:32 +0000

    Shift cleanup

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-
 
 Schedule::command('board:fetch-calendar-events')->everyMinute();
 Schedule::command('board:fetch-weather')->everyFifteenMinutes();
