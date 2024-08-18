c11cdb3f53ff4a892b01e408072290b36ecd65ac
Andy Newhouse <hi@andymnewhouse.me>
2024-08-18 13:11:07 -0500

    Create snail cluster

---
+++ app/Filament/Clusters/Snails.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class Snails extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-snails';
+}
